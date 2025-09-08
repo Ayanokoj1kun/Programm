fbdbccb643c9f76597c0379e01da927e42f693b0
Ayanokoj1kun <lmkiyotaka2@gmail.com>
2025-09-08 16:20:10 +0300

    Ichi.cs

--- Ichi.cs
+++ Ichi.cs
@@ -12,4 +12,19 @@ Console.WriteLine(Math.Pow(a + b, 2));
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 Console.WriteLine(Math.Pow(a - b, 3));
-
+//Yon 4
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(a * b * c);
+//Go 5 
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine((a + b)*0.4);
+//Roku 6
+int brutto = int.Parse(Console.ReadLine());
+Console.WriteLine(brutto/1.23);
+//Nana 7
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a % b);

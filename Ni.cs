99575c869986ff289d2f23fbfbc4ceb08a4abeec
Ayanokoj1kun <lmkiyotaka2@gmail.com>
2025-09-08 16:44:59 +0300

    Ni.cs

--- Ni.cs
+++ Ni.cs
@@ -8,3 +8,21 @@ else
 {
     Console.WriteLine("NIE");
 }
+//Ni 2
+int a = int.Parse(Console.ReadLine());
+if(100 <= a && a <= 999)
+{
+    Console.WriteLine("Jest trzy cyfrowa");
+}
+else
+{
+    Console.WriteLine("Nie jest tzycefrowa");
+}
+if(a%17 == 0)
+{
+    Console.WriteLine("Podzielna przez 17");
+}
+else
+{
+    Console.WriteLine("Nie jest podzielna przez 17");
+}

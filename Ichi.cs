1c3680273f089df529323493d1e72c44d3942e11
Ayanokoj1kun <lmkiyotaka2@gmail.com>
2025-09-08 16:07:47 +0300

    Update Ichi.cs

--- Ichi.cs
+++ Ichi.cs
@@ -2,3 +2,8 @@
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 Console.WriteLine(Math.Pow(a, 2) + Math.Pow(b, 2));
+//San
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(a - b, 3));
+
